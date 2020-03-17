51a3291633b21fb831220ecafb98254ffe198478
Pierre Lemay <pierre.lemay@inspq.qc.ca>
2020-03-16 21:56:36 -0400

    Ajout de la phage

---
+++ phage/assets/donnees/test/hockey.ts
@@ -0,0 +1,6 @@
+export const donneesTestHockey: string = 
+"Nom;Ville/État;Nombre de coupe Stanley remportée\n"
++ "Canadiens;Montréal;24\n"
++ "Maple Leafs;Toronto;13\n"
++ "Red Wings;Détroit;11"
+
